60d77b3360f6d97064d74fd35fa088955ad94563
Mrithula-spec <mrithulabala1@gmail.com>
2026-01-07 17:51:46 +0530

    Create messages.js

---
+++ backend/constants/messages.js
@@ -0,0 +1,7 @@
+export const MESSAGES = {
+  USER_EXISTS: "User already exists",
+  INVALID_CREDENTIALS: "Invalid email or password",
+  UNAUTHORIZED: "Not authorized",
+  BOOKLIST_NOT_FOUND: "Booklist not found",
+  INVALID_INPUT: "Invalid input"
+}
