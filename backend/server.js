471f769110868a1278de328ab30f778e32a5216e
Mrithula-spec <mrithulabala1@gmail.com>
2026-01-09 20:18:29 +0530

    Update server.js
    Added static file serving for images.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js"
 import authRoutes from "./routes/authRoutes.js"
 import booklistRoutes from "./routes/booklistRoutes.js"
@@ -10,9 +12,12 @@ dotenv.config()
 connectDB()
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors())
 app.use(express.json())
+app.use("/image", express.static(path.join(__dirname, "public/image")))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/booklists", booklistRoutes)
