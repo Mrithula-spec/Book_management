6f660d526a7a30e2395c9c18f6013df7c04a8167
Mrithula-spec <mrithulabala1@gmail.com>
2026-01-09 20:01:35 +0530

    Update axios configuration and token handling

--- axios.js
+++ axios.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-})
+  baseURL: "http://localhost:5000/api",
+});
 
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem("user"))
-  if (user?.token) {
-    config.headers.Authorization = `Bearer ${user.token}`
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-  return config
-})
+  return config;
+});
 
-export default api
+export default api;
