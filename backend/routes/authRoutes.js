d07ff454574a7c21a8df960f8cd4a7aa14ad2da6
Mrithula-spec <mrithulabala1@gmail.com>
2026-01-19 20:59:48 +0530

    Update authRoutes.js
    Return full user object along with JWT token upon successful registration.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -22,15 +22,22 @@ router.post("/register", async (req, res) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  await User.create({
+   const user = await User.create({
     name,
     email,
     password: hashedPassword,
   });
 
-  res.status(201).json({ message: "Registration successful" });
-});
+  // RETURN full user object + token
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    token
+  });
+});
 // Login
 router.post("/login", async (req, res) => {
   try {
