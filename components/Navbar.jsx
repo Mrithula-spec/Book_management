e544372bc50a7cb6fb9b8c652bb5e2b7b8ac7827
Mrithula-spec <mrithulabala1@gmail.com>
2026-02-02 19:31:59 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom"
 import { useContext } from "react"
+import { useLocation } from "react-router-dom";
 import logo from "../assets/logo.png"
 import { AuthContext } from "../context/AuthContext"
 
 function Navbar() {
   const { user, logout } = useContext(AuthContext)
+  const location = useLocation();
+
+if (location.pathname === "/login" || location.pathname === "/register") {
+  return null;
+}
+
 
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
-      <div className="flex items-center gap-2">
+      <Link to="/home" className="flex items-center gap-2">
   <img
     src={logo}
     alt="Book Management Logo"
     className="h-20 w-20 object-contain"
   />
   <h1 className="text-xl font-bold">Book Management System</h1>
-</div>
-
-
+</Link>
       <div className="space-x-4">
-        <Link to="/">Home</Link>
+        
         {user ? (
           <>
             <Link to="/dashboard">Dashboard</Link>
