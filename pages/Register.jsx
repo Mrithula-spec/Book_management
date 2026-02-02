4c45981ca81d4af576d25a18748dcdb5cdab492a
Mrithula-spec <mrithulabala1@gmail.com>
2026-02-02 19:28:14 +0530

    Update Register.jsx

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import api from "../api/axios"
 import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom";
+
 
 function Register() {
   const [form, setForm] = useState({ name: "", email: "", password: "" })
@@ -9,9 +11,16 @@ function Register() {
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value })
   }
+  const isValidEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+     if (!isValidEmail(form.email)) {
+    alert("Please enter a valid email (example@gmail.com)")
+    return
+  }
 
     try {
       await api.post("auth/register", form)
@@ -49,11 +58,19 @@ function Register() {
           placeholder="Password"
           onChange={handleChange}
           className="w-full mb-4 p-2 border rounded"
+          required
         />
 
         <button className="w-full bg-indigo-600 text-white p-2 rounded hover:bg-indigo-700">
           Register
         </button>
+        <p className="text-center mt-4 text-sm">
+  Already have an account?{" "}
+  <Link to="/login" className="text-indigo-600 underline">
+    Login
+  </Link>
+</p>
+
       </form>
     </div>
   )
